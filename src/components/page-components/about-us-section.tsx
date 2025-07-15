cd3956ecad60d0f982973ddbcbc6a232a749a2d7
Oindil <oindilgolder303@gmail.com>
2025-07-15 17:18:43 +0530

    lint fix

--- src/components/page-components/about-us-section.tsx
+++ src/components/page-components/about-us-section.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 import { ArrowRight } from "lucide-react";
 
 export function AboutUsSection() {
