2264a7f8044559fa39bff7bbee01604f9bf7361c
Oindil <oindilgolder303@gmail.com>
2025-07-15 14:47:43 +0530

    feat: added easygodocs-better-auth page with some fixes in json data in db

--- src/app/(main)/easygodocs-better-auth/page.tsx
+++ src/app/(main)/easygodocs-better-auth/page.tsx
@@ -1,7 +1,13 @@
+import DocumentationPage from "@/components/documentation/documentation-component";
+import docData from "@/db/ai-introduction-db.json";
 import React from "react";
 
 function page() {
-  return <div>Better Auth</div>;
+  return (
+    <div>
+      <DocumentationPage docData={docData} />
+    </div>
+  );
 }
 
 export default page;
