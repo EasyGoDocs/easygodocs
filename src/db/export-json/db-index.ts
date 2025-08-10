4bf5ea50a829c3f8d55246c2d631185fcba17a68
itsabhinavm <abhinav051017@gmail.com>
2025-08-10 12:00:50 +0530

    bugfix: build fail in eslint

--- src/db/export-json/db-index.ts
+++ src/db/export-json/db-index.ts
@@ -1,16 +1,8 @@
 // AUTO-GENERATED FILE. DO NOT EDIT.
 // Run scripts/generate-db-index.js to update.
 
-import doc0 from "../ai-introduction-db.json";
-import doc1 from "../better-auth-db.json";
-import doc2 from "../react.json";
-import doc3 from "../ubuntu-installation.json";
-import doc4 from "../vercel-build-error.json";
+import doc0 from "../react.json";
 
 export const dbFiles = [
   doc0,
-  doc1,
-  doc2,
-  doc3,
-  doc4,
 ];
