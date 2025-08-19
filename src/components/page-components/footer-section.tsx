7e1bb1b8c155700eded5e924dd0224bc347a4f89
Oindil <oindilgolder303@gmail.com>
2025-08-19 10:15:44 +0530

    chore: footer color changed

--- src/components/page-components/footer-section.tsx
+++ src/components/page-components/footer-section.tsx
@@ -76,12 +76,12 @@ const Logo = ({ className }: { className?: string }) => (
 
 export function Footer() {
   return (
-    <footer className="md:rounded-t-6xl relative w-full flex flex-col items-center justify-center rounded-t-4xl border-t bg-[radial-gradient(35%_128px_at_50%_0%,theme(backgroundColor.white/8%),transparent)] px-6 py-12 lg:py-16">
+    <footer className="md:rounded-t-6xl relative w-full flex flex-col items-center justify-center rounded-t-4xl border-t bg-[radial-gradient(35%_128px_at_50%_0%,theme(backgroundColor.white/8%),transparent)] px-6 py-12 lg:py-16 bg-white dark:bg-black">
       <div className="bg-foreground/20 absolute top-0 right-1/2 left-1/2 h-px w-1/3 -translate-x-1/2 -translate-y-1/2 rounded-full blur" />
 
       <div className="grid w-full gap-8 xl:grid-cols-3 xl:gap-8">
         <AnimatedContainer className="space-y-4">
-          <Logo/>
+          <Logo />
           <p className="text-muted-foreground mt-8 text-sm md:mt-0">
             © {new Date().getFullYear()} EasyGoDocs. All rights reserved.
           </p>
